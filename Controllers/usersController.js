b049942a258e872be1c0ba06c62a7d8c94f43c3c
Glody Mafo <glodymafokis@gmail.com>
2024-02-06 14:48:18 +0200

    resolving

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -1,53 +1,53 @@
-const users=[];
+const users = [];
 
-exports.showUsers=(req, res) => {
+exports.showUsers = (req, res) => {
     res.json(users);
-  }
+}
 
 // Ajouter un nouvel utilisateur
 
-exports.postUser=(req, res)=>{
+exports.postUser = (req, res) => {
     const newUser = req.body;
     newUser.id = users.length + 1;
     users.push(newUser);
-    res.json(newUser);  
+    res.json(newUser);
 }
 
 // Afficher un utilisateur par son id
 
-exports.showUserById=(req, res) => {
+exports.showUserById = (req, res) => {
     const userId = parseInt(req.params.id);
     const user = users.find((user) => user.id === userId);
     if (!user) {
-      res.status(404).send("l'utilisateur est introuvable");
-      return;
+        res.status(404).send("l'utilisateur est introuvable");
+        return;
     }
     res.json(user);
-  }
+}
 
 // Modification les information d'un utilisateur
 
-exports.editUser=(req, res) => {
+exports.editUser = (req, res) => {
     const userId = parseInt(req.params.id);
     const updatedUser = req.body;
     const user = users.find((user) => user.id === userId);
     if (!user) {
-      res.status(404).send('Utilisateur introuvable');
-      return;
+        res.status(404).send('Utilisateur introuvable');
+        return;
     }
     Object.assign(user, updatedUser);
     res.json(user);
-  }
+}
 
 // Supprimer un compte utilisateur
 
-exports.deleteTweet=(req, res) => {
-    const tweetId = parseInt(req.params.id);
-    const tweetIndex = users.findIndex((tweet) => tweet.id === tweetId);
-    if (tweetIndex === -1) {
-      res.status(404).send('Tweet introuvable');
-      return;
+exports.deleteUser = (req, res) => {
+    const userId = parseInt(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === userId);
+    if (userIndex === -1) {
+        res.status(404).send('Tweet introuvable');
+        return;
     }
     users.splice(tweetIndex, 1);
-    res.json({"message": "Tweet supprimé"});
-  };
+    res.json({ "message": "L'utilisateur est supprimé avec succès" });
+};
