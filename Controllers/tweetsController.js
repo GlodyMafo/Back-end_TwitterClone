3dfbf0f9df46e56adc911e2a28a429732be8d103
Glody Mafo <glodymafokis@gmail.com>
2024-02-06 17:36:28 +0200

    daily update

--- Controllers/tweetsController.js
+++ Controllers/tweetsController.js
@@ -1,6 +1,16 @@
-// Lire tous les tweets
+const tweets = [
+  {
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+      "url": "https://via.placeholder.com/600/92c952",
+      "thumbnailUrl": "https://via.placeholder.com/150/92c952",
+      "like": 9122,
+      "repost": 10
+    }
+]
 
-const tweets = [];
+// Lire tous les tweets
 
 exports.showTweet = (req, res) => {
   res.json(tweets);
